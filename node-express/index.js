7e45f85ccdf09f907506fb1916ec6240be9e0fa7
Emmanuel Ahuno <manuelkpatsu@gmail.com>
2023-01-03 15:10:40 +0000

    Handle API requests on sub-documents for dish document

--- node-express/index.js
+++ node-express/index.js
@@ -27,6 +27,17 @@ app.use('/dishes', dishRouter)
 app.use('/promotions', promoRouter)
 app.use('/leaders', leaderRouter)
 
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : ''
+
+  // render the error page
+  res.status(err.status || 500)
+  res.render('error')
+})
+
 const server = http.createServer(app)
 
 server.listen(port, hostname, () => {
