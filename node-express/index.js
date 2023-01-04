b8038cc4af2f7ecf3ad7bedef10a8e7ffc9d047a
Emmanuel Ahuno <manuelkpatsu@gmail.com>
2023-01-04 09:01:18 +0000

    Set up basic authentication

--- node-express/index.js
+++ node-express/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const http = require('http')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
+const cookieParser = require('cookie-parser')
 
 const dishRouter = require('./routes/dishRouter')
 const promoRouter = require('./routes/promoRouter')
@@ -22,6 +23,37 @@ connect.then((db) => {
 app.use(morgan('tiny'))
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+app.use(cookieParser())
+
+function auth(req, res, next) {
+  var authHeader = req.headers.authorization
+
+  if (!authHeader) {
+    var err = new Error('You are not authenticated!')
+
+    res.setHeader('WWW-Authenticate', 'Basic')
+    err.status = 401
+    next(err)
+    return
+  }
+
+  var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':')
+
+  var username = auth[0]
+  var password = auth[1]
+
+  if (username === 'admin' && password === 'password') {
+    next()
+  } else {
+    var err = new Error('You are not authenticated!')
+
+    res.setHeader('WWW-Authenticate', 'Basic')
+    err.status = 401
+    next(err)
+  }
+}
+
+app.use(auth)
 
 app.use('/dishes', dishRouter)
 app.use('/promotions', promoRouter)
